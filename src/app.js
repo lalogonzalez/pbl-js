99193e90c5b8506f7c0e3e5f75afec27cd927ab7
Eduardo <eduardo_gzz@outlook.com>
2016-02-20 23:03:23 -0600

    Avances at Last!
    useful shit
    comments
    aux_window

--- src/app.js
+++ src/app.js
@@ -1,13 +1,37 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
+/*
+*	Eduardo Gonzalez
+*	Miguel Sepulveda
+*	Rodolfo Montes
+/*
 
-var UI = require('ui');
-var Vector2 = require('vector2');
+
+/*
+*	AJAX
+*/
+/*
+var ajax = require('ajax');
+ajax(
+  {
+    url: 'http://api.theysaidso.com/qod.json',
+    type: 'json'
+  },
+  function(data, status, request) {
+    console.log('Quote of the day is: ' + data.contents.quote);
+  },
+  function(error, status, request) {
+    console.log('The ajax request failed: ' + error);
+  }
+);
+*/
+
+
+
+/*
+*		MISC
+*/
+var UI = require('ui');					//user interface
+var Vector2 = require('vector2');	//arrays
 var id;
-var card_location= new UI.Card();
 
 var locationOptions = {
   enableHighAccuracy: true, 
@@ -15,20 +39,70 @@ var locationOptions = {
   timeout: 5000
 };
 
-function locationSuccess(pos) {
-  console.log('Location changed!');
-  console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
-  card_location.title('It works');
-  card_location.subtitle('Location');
-  card_location.body('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
-  card_location.show();
-}
+var aux_window = null;
 
-function forcedLocationSuccess(pos) {
-  card_location.title('Forced');
-  card_location.subtitle('Location');
-  card_location.body('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
-  card_location.show();
+
+/*
+*		CATEGORIAS
+*/
+var categorias = [
+  {
+    title: "Restaurantes",
+    subtitle: "fast food, etc"
+  },
+  {
+    title: "Electronicos",
+    subtitle: "Computadoras, Videojuegos, etc"
+  },
+  {
+    title: "Salud",
+    subtitle: "Hospitales, Farmacias"
+  },
+  {
+  	title: "Ropa",
+  	subtitle: "Camisas, abrigos, etc"
+  },
+  {
+  	title: "Parques",
+  	subtitle: "Recreativos, Deportes, etc"
+  }
+];
+
+var menuCategorias = new UI.Menu({
+  sections: [{
+    title: 'Lista Categorias',
+    items: categorias
+  }]
+});
+
+var lugar = '{ "nombre":"La Dream Tienda","Distancia":100,"Rating":85,"Categoria":"Electronicos"}';
+
+
+/*
+*		MAIN
+*/
+var main = new UI.Card({
+  	title: 'Street Assistant',
+  	icon: 'images/menu_icon.png',
+  	subtitle: 'Ubica lugares cercanos!',
+  	body: 'Selecciona las categorias de tu interes y recibe alertas cuando te encuentres cerca de algun lugar de tu posible interes. *Nota: Conserva la aplicacion abierta para recibir notificaciones.',
+	scrollable: true
+});
+
+aux_window = main;
+aux_window.show();
+
+
+/*
+*		LOCALIZACION
+*/
+function locationSuccess(pos) {
+	//CHECAR LA BASE DE DATOS
+	aux_window.show();
+	
+	// LOG DEBUG - SHOW LOCATION
+  	console.log('Location changed!');
+  	console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
 }
 
 function locationError(err) {
@@ -45,53 +119,24 @@ Pebble.addEventListener('ready',
 
 navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
-});
+aux_window.show();
 
-main.show();
 
+/*
+*		EVENTOS - BOTONES
+*/
 main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+	//MOSTRAR VENTANA ANTERIOR
 });
 
 main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
+	aux_window = menuCategorias;
+	aux_window.show();
 });
 
 main.on('click', 'down', function(e) {
-  navigator.geolocation.getCurrentPosition(forcedLocationSuccess, locationError, locationOptions);
-  
+	//MOSTRAR VENTANA SIGUIENTE
 });
+
+
+
